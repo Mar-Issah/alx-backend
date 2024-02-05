991f2120789700b49f3104ef56f0a93427c83ab2
Mar-Issah <masy370@gmail.com>
2024-02-05 13:12:34 -0600

    Add new file 9-stock.js

--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -14,16 +14,28 @@ redisClient.on('err', (err) => {
 
 const listProducts = [
   {
-    Id: 1, name: 'Suitcase 250', price: 50, stock: 4,
+    Id: 1,
+    name: 'Suitcase 250',
+    price: 50,
+    stock: 4,
   },
   {
-    Id: 2, name: 'Suitcase 450', price: 100, stock: 10,
+    Id: 2,
+    name: 'Suitcase 450',
+    price: 100,
+    stock: 10,
   },
   {
-    Id: 3, name: 'Suitcase 650', price: 350, stock: 2,
+    Id: 3,
+    name: 'Suitcase 650',
+    price: 350,
+    stock: 2,
   },
   {
-    Id: 4, name: 'Suitcase 1050', price: 550, stock: 5,
+    Id: 4,
+    name: 'Suitcase 1050',
+    price: 550,
+    stock: 5,
   },
 ];
 
@@ -67,29 +79,29 @@ app.get('/list_products', (req, res) => {
   res.json(getItems());
 });
 
-app.get('/list_products/:itemId', async (req, res) => {
-  const itemId = Number(req.params.itemId);
+app.get('/list_products/:itemId', async (request, response) => {
+  const itemId = Number(request.params.itemId);
   const item = getItemById(itemId);
   if (Object.values(item).length > 0) {
     const stock = await getCurrentReservedStockById(itemId);
     item.currentQuantity = item.initialAvailableQuantity - stock;
-    return res.json(item);
+    return response.json(item);
   }
-  return res.json({ status: 'Product not found' });
+  return response.json({ status: 'Product not found' });
 });
 
-app.get('/reserve_product/:itemId', async (req, res) => {
-  const itemId = Number(req.params.itemId);
+app.get('/reserve_product/:itemId', async (request, response) => {
+  const itemId = Number(request.params.itemId);
   const item = getItemById(itemId);
   if (Object.values(item).length === 0) {
-    return res.json({ status: 'Product not found' });
+    return response.json({ status: 'Product not found' });
   }
   const stock = await getCurrentReservedStockById(itemId);
   if (stock >= item.initialAvailableQuantity) {
-    return res.json({ status: 'Not enough stock available', itemId });
+    return response.json({ status: 'Not enough stock available', itemId });
   }
   await reserveStockById(itemId, Number(stock) + 1);
-  return res.json({ status: 'Reservation confirmed', itemId });
+  return response.json({ status: 'Reservation confirmed', itemId });
 });
 
 function clearRedisStock() {
