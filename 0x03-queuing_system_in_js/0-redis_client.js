34b75c35356bb27e2b832ab481a2e48a786608e0
Mar-Issah <masy370@gmail.com>
2024-02-05 12:44:13 -0600

    Add new file 0-redis_client.js files

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+import { createClient } from 'redis';
+/**
+ * Node redis client
+ */
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
